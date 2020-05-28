225366130c9898b6189c4dc3d6192c952db101a5
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2020-05-28 14:57:14 +0200

    Add logging

--- src/main/java/nl/knaw/dans/kb/resolver/UrlResolver.java
+++ src/main/java/nl/knaw/dans/kb/resolver/UrlResolver.java
@@ -1,5 +1,8 @@
 package nl.knaw.dans.kb.resolver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -8,23 +11,13 @@ import java.net.URLConnection;
 
 public class UrlResolver {
 
-  public static void main(String[] args) {
-
-    String[] locations = { "http://dds.nl", "http://localhost:8081/redir", "http://disq.us/8o29ub", "http://bit.ly/1Wd7H3h", "http://httpstat.us/500", "http://httpstat.us/405", "https://www.persistent-identifier.nl/urn:nbn:nl:ui:12-d68990f1-78fb-4c7d-a407-45477ac821ad", "https://pure.buas.nl/en/publications/d8bf9616-8cd9-4b1f-ad3e-e3277b92adfa", "http://resolver.tudelft.nl/uuid%3Afcf57b9b-1a1d-410b-9e85-08c8fc72d93b?steiny=gek" };
-
-    for (String location : locations) {
-      int statuscode = UrlResolver.getResponseCode(location, true);
-      System.out.println("RESOLVED code: " + statuscode);
-    }
-  }
-
-  //  private static final Logger LOG = LoggerFactory.getLogger(ResolveIdentifierController.class);
-
   private static final int MAX_HOP_COUNT = 10;
   private static final String HEADER_FIELD_LOCATION = "Location";
-  private static final String user_agent = "nbn-resolver/0.1.0 - harvester@dans.knaw.nl";
+  private static final String user_agent = "nbn-urlresolver - harvester@dans.knaw.nl";
   public static final String METHOD_HEAD = "HEAD";
   public static final String METHOD_GET = "GET";
+
+  private static final Logger logger = LoggerFactory.getLogger(UrlResolver.class);
 
   public static int getResponseCode(String urlString, boolean followredirects) {
     return UrlResolver.getResponseCode(urlString, 0, followredirects, METHOD_HEAD);
@@ -35,7 +28,7 @@ public class UrlResolver {
    *
    * @param urlString       the url string
    * @param cnt_redir       the redirected number counter
-   * @param followredirects a boolean indicating whether or not to follow HTTP redirects.
+   * @param followredirects a boolean indicating whether or not to follow HTTP redirects by the java.net.URLConnection library.
    * @param method          the HTTP method
    * @return the HTTP response code or -1 if no connection can be established (URL does not exist) OR @param cnt_redir number exeeded.
    */
@@ -51,30 +44,31 @@ public class UrlResolver {
       if (connection instanceof HttpURLConnection) {
         HttpURLConnection httpConnection = (HttpURLConnection) connection;
         httpConnection.setRequestMethod(method);
-        httpConnection.setInstanceFollowRedirects(false);  //it only applies to redirects to the same protocol. An HttpURLConnection won't automatically follow redirects to a different protocol, even if the redirect flag is set.
+        // Setting #.setInstanceFollowRedirects(true) is faster and probably more reliaable, since we don't have to follow the redirs ourselves. However, this hides the redirect url-hubs from this method. It still requires 'manual' action if the redirect encounters a protocol change (http => hhtps).
+        httpConnection.setInstanceFollowRedirects(followredirects);  //it only applies to redirects to the same protocol. An HttpURLConnection won't automatically follow redirects to a different protocol, even if the redirect flag is set.
         httpConnection.addRequestProperty("User-Agent", user_agent);
         httpConnection.connect();
         response_code = httpConnection.getResponseCode();
 
         if (response_code == HttpURLConnection.HTTP_MOVED_PERM || response_code == HttpURLConnection.HTTP_MOVED_TEMP || response_code == HttpURLConnection.HTTP_SEE_OTHER) {
           String movedLocation = httpConnection.getHeaderField(HEADER_FIELD_LOCATION);
-          System.out.println(method + ": " + urlString + " (" + response_code + ") => Location-Header: " + movedLocation);
+          //          System.out.println(method + ": " + urlString + " (" + response_code + ") => Location-Header: " + movedLocation);
           if (movedLocation != null && !movedLocation.isEmpty() && cnt_redir <= MAX_HOP_COUNT) {
             movedLocation = mergePaths(urlString, movedLocation);
             response_code = getResponseCode(movedLocation, (cnt_redir + 1), true, METHOD_HEAD);
           }
         }
         else if (response_code == HttpURLConnection.HTTP_BAD_METHOD && method.equalsIgnoreCase(METHOD_HEAD)) {
-          System.out.println(method + ": " + urlString + " (" + response_code + ") => Re-try with : " + METHOD_GET);
+          //          System.out.println(method + ": " + urlString + " (" + response_code + ") => Re-try with : " + METHOD_GET);
           response_code = getResponseCode(urlString, (cnt_redir + 1), true, METHOD_GET);
         }
         else { //Received other than 30x or 405 response. We're finished.
-          System.out.println(method + ": " + urlString + " (" + response_code + ") => Re-tries : " + cnt_redir + ", realURL: " + httpConnection.getURL());
+          logger.info("Unresolvable: " + urlString + " (" + response_code + ") => Re-tries : " + cnt_redir + ", realURL: " + httpConnection.getURL());
         }
       }
     }
     catch (IOException e) {
-      System.out.println("Could not retrieve response for [" + urlString + "], IOException msg: " + e.getMessage());
+      logger.info("Could not retrieve response for [" + urlString + "], IOException msg: " + e.getMessage());
     }
     return response_code;
   }
@@ -85,7 +79,7 @@ public class UrlResolver {
       try {
         URL base = new URL(baseurl);
         URL newone = new URL(base, newlocation);
-        System.out.println(newone.toString());
+        //        System.out.println(newone.toString());
         return newone.toString();
       }
       catch (MalformedURLException e) {
