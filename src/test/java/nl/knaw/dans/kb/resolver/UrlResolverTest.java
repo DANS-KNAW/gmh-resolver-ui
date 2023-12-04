0237601e37a3d234521a239753c6fb6402eac169
wilkos-dans <wilko.steinhoff@dans.knaw.nl>
2023-12-04 14:31:54 +0100

    Updated test expects

--- src/test/java/nl/knaw/dans/kb/resolver/UrlResolverTest.java
+++ src/test/java/nl/knaw/dans/kb/resolver/UrlResolverTest.java
@@ -24,8 +24,8 @@ import static org.junit.Assert.assertEquals;
 public class UrlResolverTest {
 
   private static final Logger logger = LoggerFactory.getLogger(UrlResolverTest.class);
-  String[] locations = { "http://dds.nl", "http://localhost:8081/redir", "http://disq.us/8o29ub", "http://bit.ly/1Wd7H3h", "http://httpstat.us/500", "http://httpstat.us/405", "https://www.persistent-identifier.nl/urn:nbn:nl:ui:12-d68990f1-78fb-4c7d-a407-45477ac821ad", "https://pure.buas.nl/en/publications/d8bf9616-8cd9-4b1f-ad3e-e3277b92adfa", "http://resolver.tudelft.nl/uuid%3Afcf57b9b-1a1d-410b-9e85-08c8fc72d93b?some=param" };
-  int[] statuscodes = {200, -1, 410, -1, 500, 405, 200, 200, 200};
+  String[] locations = { "http://dds.nl", "http://disq.us/8o29ub", "http://bit.ly/1Wd7H3h", "http://httpstat.us/500", "http://httpstat.us/405", "https://www.persistent-identifier.nl/urn:nbn:nl:ui:12-d68990f1-78fb-4c7d-a407-45477ac821ad", "https://pure.buas.nl/en/publications/d8bf9616-8cd9-4b1f-ad3e-e3277b92adfa", "http://resolver.tudelft.nl/uuid%3Afcf57b9b-1a1d-410b-9e85-08c8fc72d93b?some=param" };
+  int[] statuscodes = {200, 404, -1, 500, 405, 200, 200, 404};
 
   @Test
   public void testResolveRealUrls() {
@@ -33,8 +33,8 @@ public class UrlResolverTest {
     int i = 0;
     for (String location : locations) {
       int statuscode = UrlResolver.getResponseCode(location, true);
-      assertEquals(statuscode, statuscodes[i]);
-      System.out.println( statuscode + ": " + location);
+      assertEquals(statuscodes[i], statuscode);
+      System.out.println(statuscode + ": " + location);
       i++;
     }
   }
